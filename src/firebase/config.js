807c30360f3d7f3e47ff0b15013fe5ad3ff0b05f
IbrahimHastorun <113854037+IbrahimHastorun@users.noreply.github.com>
2023-10-18 09:17:32 +0300

    React Firebase Tailwind Css Hospital Management System

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    // YOUR FIREBASE PROJECT CONFIG INFO
+    apiKey : process.env.REACT_APP_API_KEY ,
+    authDomain : process.env.REACT_APP_AUTH_DOMAIN ,
+    projectId : process.env.REACT_APP_PROJECT_ID ,
+    storageBucket : process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId : process.env.REACT_APP_MESSAGING_SENDER_ID ,
+    appId : process.env.REACT_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
